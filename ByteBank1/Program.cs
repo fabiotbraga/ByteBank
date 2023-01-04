528230b40005ca2646449526e2bb9c9e1f72081f
fabiotbraga <fabiotbraga@gmail.com>
2023-01-04 11:39:07 -0300

    :recycle: refactor: Refatorando conforme as novas especificações do projeto

--- ByteBank1/Program.cs
+++ ByteBank1/Program.cs
@@ -2,46 +2,38 @@
 
 public class Program
 {
-    static void ShowMenu() {
-        Console.WriteLine("1 - Inserir novo usuário");
-        Console.WriteLine("2 - Deletar um usuário");
-        Console.WriteLine("3 - Detalhes de um usuário");
-        Console.WriteLine("4 - Total armazenado no banco");
-        Console.WriteLine("0 - Para sair do programa");
-        Console.Write("Digite a opção desejada: ");
+    static void Init()
+    {
+        Console.WriteLine("Bem vindo ao ByteBank! Por favor, digite a opção desejada:");
+        Console.WriteLine("1 - Já sou usuário e desejo entrar em minha conta");
+        Console.WriteLine("2 - Sou novo e desejo criar uma conta");
+        Console.WriteLine("0 - Para sair do programa");  
     }
 
-    static void CreateUser()
+    static void CreateUser(List<string> titular, List<string> cpf, List<decimal> saldo)
     {
-        
+        throw new NotImplementedException();
     }
+    
     public static void Main(string[] args)
     {
-        Console.WriteLine("Antes de começar a usar, vamos configurar alguns valores: ");
-
-        Console.Write("Digite a quantidade de Usuários: ");
-        int quantidadeDeUsuarios = int.Parse(Console.ReadLine());
-
-        int option;
-
-        do {
-            ShowMenu();
-            option = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("-----------------");
-
-            switch (option) {
-                case 0:
-                    Console.WriteLine("Estou encerrando o programa...");
-                    break;
-                case 1:
-                    Console.WriteLine("Deveria estar inserindo um novo usario!");
-                    break;
-            }
-
-            Console.WriteLine("-----------------");
-
-        } while (option != 0);
+        int optionInit;
+        do
+        {
+           Init(); 
+           optionInit = int.Parse(Console.ReadLine());
+           switch (optionInit) {
+               case 0:
+                   Console.WriteLine("Estou encerrando o programa...");
+                   break;
+               case 1:
+                   Console.WriteLine("Entrar na conta");
+                   break;
+               case 2:
+                   Console.WriteLine("Criar nova conta");
+           }
+   
+        } while (optionInit != 0);
     }
 }
 
