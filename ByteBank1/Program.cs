0635f2a9101df68aa776098bd23a767111c04574
fabiotbraga <fabiotbraga@gmail.com>
2023-01-04 14:49:34 -0300

    :sparkles: feat: Adicionando a adicionando as funcoes de saque, deposito e transferencia

--- ByteBank1/Program.cs
+++ ByteBank1/Program.cs
@@ -6,22 +6,25 @@ public class Program
 {
     static void Init()
     {
+        //Console.Clear();
         Console.WriteLine("Bem vindo ao ByteBank! Por favor, digite a opção desejada:");
         Console.WriteLine("1 - Já sou usuário e desejo entrar em minha conta");
         Console.WriteLine("2 - Sou novo e desejo criar uma conta");
+        Console.WriteLine("3 - Ver conta");
         Console.WriteLine("0 - Para sair do programa");  
     }
 
-    static void Login(List<string> cpfs, List<string> senhas)
+    static void Login(List<User> contas)
     {
         Console.Clear();
         
         Console.WriteLine("Digite o seu cpf:");
         string cpfLogin = Console.ReadLine();
-        int cpfFind = cpfs.FindIndex(cpf => cpf == cpfLogin);
+        int cpfFind = contas.FindIndex(cpf => cpf == contas.Find( a => a.Cpf == cpfLogin ));
+        
         Console.WriteLine("Digite sua senha");
         string senhaLogin = Console.ReadLine();
-        int senhaFind = senhas.FindIndex(senhas => senhas == senhaLogin);
+        int senhaFind = contas.FindIndex(senhas => senhas == contas.Find(a => a.Senha == senhaLogin));
         
         if(cpfFind == -1) {
             Console.WriteLine("Não foi possível encontrar esta Conta");
@@ -31,15 +34,34 @@ public class Program
             Console.WriteLine("Não foi possível encontrar esta Conta");
             Console.WriteLine("MOTIVO: Senha não encontrada.");
         }
+        
+        Console.Clear();
+        Operations();
     }
 
     static void Operations()
     {
+        int option;
         Console.WriteLine("Por favor, digite a operação que deseja realizar:");
         Console.WriteLine("1 - Saque");
         Console.WriteLine("2 - Depósito");
-        Console.WriteLine("3 - Ver conta");
         Console.WriteLine("0 - Para sair do programa");
+
+        option = int.Parse(Console.ReadLine());
+        switch (option) {
+            case 0:
+                Console.WriteLine("Estou encerrando o programa...");
+                break;
+            case 1:
+                Saque();
+                break;
+            case 2:
+                Deposito();
+                break;
+            case 3:
+                Transferencia();
+                break;
+        }
     }
     
     static void CreateUser(List<User> contas)
@@ -60,6 +82,7 @@ public class Program
     
     static void ApresentaConta( int index, List<User> contas) {
         Console.WriteLine($"CPF = {contas[index].Cpf} | Titular = {contas[index].Nome} | Saldo = R${contas[index].Saldo:F2}");
+        Console.Clear();
     }
     static void ApresentarUsuario(List<User> contas) {
         
@@ -77,6 +100,26 @@ public class Program
         ApresentaConta(indexParaApresentar, contas);
     }
 
+    static void Saque()
+    {
+        Console.Clear();
+        
+        Console.WriteLine("Saque");
+    }
+    
+    static void Deposito()
+    {
+        Console.Clear();
+        
+        Console.WriteLine("Deposito");
+    }
+    
+    static void Transferencia()
+    {
+        Console.Clear();
+        Console.WriteLine("Transferencia");
+    }
+
     public static void Main(string[] args)
     {
         int optionInit;
@@ -90,11 +133,10 @@ public class Program
                    Console.WriteLine("Estou encerrando o programa...");
                    break;
                case 1:
-                   //Login(titulares, senhas);
+                   Login(contas);
                    break;
                case 2:
                    CreateUser(contas);
-                   //Operations();
                    break;
                case 3:
                    ApresentarUsuario(contas);
